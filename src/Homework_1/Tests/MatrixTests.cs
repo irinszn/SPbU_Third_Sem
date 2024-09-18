cda4b74d126a1a9852ecb0ff7ddd97e57473634d
irinszn <ira.sazonova.spb@gmail.com>
2024-09-19 00:57:10 +0300

    Add: tests

---
+++ src/Homework_1/Tests/MatrixTests.cs
@@ -0,0 +1,76 @@
+namespace Tests;
+
+using Matrices;
+
+public class MatrixTests
+{
+    [Test]
+    public void ReadMatrixFromFile_WorksCorrectly()
+    {
+        int[,] expected = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+        var matrix = TransformMatrix.ReadMatrixFromFile("../../../Files/MatrixWithManySpaces.txt");
+
+        Assert.AreEqual(expected, matrix.Values);
+    }
+
+    [Test]
+    public void WriteMatrixToFile_WorksCorrectly()
+    {
+        var matrix = new Matrix(3, 3);
+        int[,] values = { {1, 2, 3}, {4, 5, 6}, {7, 8, 9} };
+
+        matrix.Values = values;
+        TransformMatrix.WriteMatrixToFile(matrix);
+
+        Assert.AreEqual(TransformMatrix.ReadMatrixFromFile("result.txt").Values, TransformMatrix.ReadMatrixFromFile("../../../Files/Write_result.txt").Values);
+    }
+
+    [TestCase("../../../Files/matrix_1.txt", "../../../Files/matrix_2.txt", "../../../Files/result_12.txt")]
+    [TestCase("../../../Files/matrix_11.txt", "../../../Files/matrix_22.txt", "../../../Files/result_1122.txt")]
+
+    public void Multiplication_WorksCorrectly(string matrix_one, string matrix_two, string expectedResult)
+    {
+        var matrix_1 = TransformMatrix.ReadMatrixFromFile(matrix_one);
+        var matrix_2 = TransformMatrix.ReadMatrixFromFile(matrix_two);
+        var expected = TransformMatrix.ReadMatrixFromFile(expectedResult);
+
+        var result = MatrixMultiplication.Multiplication(matrix_1, matrix_2);
+        
+        Assert.AreEqual(result.Values, expected.Values);
+    }
+
+    [TestCase("../../../Files/matrix_1.txt", "../../../Files/matrix_2.txt", "../../../Files/result_12.txt")]
+    [TestCase("../../../Files/matrix_11.txt", "../../../Files/matrix_22.txt", "../../../Files/result_1122.txt")]
+    public void ParallelMultiplication_WorksCorrectly(string matrix_one, string matrix_two, string expectedResult)
+    {
+        var matrix_1 = TransformMatrix.ReadMatrixFromFile(matrix_one);
+        var matrix_2 = TransformMatrix.ReadMatrixFromFile(matrix_two);
+        var expected = TransformMatrix.ReadMatrixFromFile(expectedResult);
+
+        var result = MatrixMultiplication.ParallelMultiplication(matrix_1, matrix_2);
+        
+        Assert.AreEqual(result.Values, expected.Values);
+    }
+
+    [Test]
+    public void ParallelMultiplication_ThrowsArgumentException_WhenMatricesNotConsistent()
+    {
+        var matrix_1  = new Matrix(3, 4);
+        var matrix_2  = new Matrix(5, 6);
+
+        Assert.Throws<ArgumentException>(() => MatrixMultiplication.ParallelMultiplication(matrix_1, matrix_2));
+    }
+
+    [Test]
+    public void ReadMatrixFromFile_ThrowsFileNotFoundException_WithWrongFilePath()
+    {
+        Assert.Throws<FileNotFoundException>(() => TransformMatrix.ReadMatrixFromFile("../../../Files/Flower.txt"));
+    }
+
+    [Test]
+    public void ReadMatrixFromFile_ThrowsArgumentException_WithEmptyFile()
+    {
+        Assert.Throws<ArgumentException>(() => TransformMatrix.ReadMatrixFromFile("../../../Files/EmptyFile.txt"));
+    }
+}
